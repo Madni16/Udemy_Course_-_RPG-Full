0444e82de09eb949eab905e9c89fee99f2254425
Madni <nico.barasinski@gmail.com>
2025-10-12 19:26:37 +0200

    Implemented On Damage VFX and added a few more folders for structure

--- Assets/Scripts/Entity_Health.cs
+++ Assets/Scripts/Entity_Health.cs
@@ -1,16 +1,23 @@
-using System;
 using UnityEngine;
 
 public class Entity_Health : MonoBehaviour
 {
+    private Entity_VFX entityVfx;
+
     [SerializeField] protected float maxHealth = 100;
     [SerializeField] protected bool isDead;
 
+    protected virtual void Awake()
+    {
+        entityVfx = GetComponent<Entity_VFX>();
+    }
+
     public virtual void TakeDamage(float damage, Transform damageDealer)
     {
-        if (isDead) 
+        if (isDead)
             return;
 
+        entityVfx?.PlayOnDamageVfx();
         ReduceHealth(damage);
     }
 
