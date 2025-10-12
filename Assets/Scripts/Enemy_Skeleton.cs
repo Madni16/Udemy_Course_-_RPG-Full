478e0b7dce75589c54de5b7bf6d27479ae6221b5
Madni <nico.barasinski@gmail.com>
2025-10-12 16:01:48 +0200

    Implemented logic for Enemy walk and idle states as well as animations for Skeleton walk and idle

--- Assets/Scripts/Enemy_Skeleton.cs
+++ Assets/Scripts/Enemy_Skeleton.cs
@@ -1,4 +1,17 @@
 public class Enemy_Skeleton : Enemy
 {
+    protected override void Awake()
+    {
+        base.Awake();
 
+        IdleState = new Enemy_IdleState(this, stateMachine, "idle");
+        MoveState = new Enemy_MoveState(this, stateMachine, "move");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        stateMachine.Initialize(IdleState);
+    }
 }
