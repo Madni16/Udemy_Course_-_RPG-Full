14d4d9c8dbace5f3a3cefa0d73ecceb677ee187d
Madni <nico.barasinski@gmail.com>
2025-10-12 17:12:40 +0200

    Improved Enemy_Battlestate.cs by implementing proper player detection and adding a chase functionality to enemy when detected player

--- Assets/Scripts/Enemy_BattleState.cs
+++ Assets/Scripts/Enemy_BattleState.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Enemy_BattleState : EnemyState
 {
+    private Transform player;
     public Enemy_BattleState(Enemy enemy, StateMachine statemachine, string animBoolName) : base(enemy, statemachine, animBoolName)
     {
     }
@@ -10,6 +11,35 @@ public class Enemy_BattleState : EnemyState
     {
         base.Enter();
 
-        Debug.Log("I entered Battle State");
+        if (player == null)
+            player = enemy.PlayerDetection().transform;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (WithinAttackRange())
+            stateMachine.ChangeState(enemy.AttackState);
+        else
+            enemy.SetVelocity(enemy.battleMoveSpeed * DirectionToPlayer(), rb.linearVelocity.y);
+    }
+
+    private bool WithinAttackRange() => DistanceToPlayer() < enemy.attackDistance;
+
+    private float DistanceToPlayer()
+    {
+        if (player == null)
+            return float.MaxValue;
+
+        return Mathf.Abs(player.position.x - enemy.transform.position.x);
+    }
+
+    private int DirectionToPlayer()
+    {
+        if (player == null)
+            return 0;
+
+        return player.position.x > enemy.transform.position.x ? 1 : -1;
     }
 }
