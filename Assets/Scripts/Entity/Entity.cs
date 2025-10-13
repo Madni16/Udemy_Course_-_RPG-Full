35b2f441932f0e36be5b2c132762058de36f374f
Madni <nico.barasinski@gmail.com>
2025-10-13 16:43:50 +0200

    Implemented health bar for enemies (and used for Player as well for now)

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    public event Action OnFlipped;
+
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
 
@@ -96,6 +99,8 @@ public class Entity : MonoBehaviour
         transform.Rotate(0f, 180f, 0f);
         facingRight = !facingRight;
         facingDir *= -1;
+
+        OnFlipped?.Invoke();
     }
 
     private void HandleCollisionDetection()
